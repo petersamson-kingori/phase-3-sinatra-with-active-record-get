a6f82c52f1a733f7350457e90282722322f358de
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-04-15 16:07:42 +0300

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+   get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+
 end
